78d9101197b135c8d0ee636131b7c1642ab7c3ff
Manuel Bogoni <28894037+manuelbogoni@users.noreply.github.com>
2022-02-26 16:07:01 +0100

    Delete friction_slope.m

--- friction_slope.m
+++
@@ -1,8 +0,0 @@
-% FRICTION SLOPE FROM GAUCLKER-STRICKLER FORMULA
-function Se = friction_slope(Ks, U, D)
-
-% energy loss per unit length
-Se = ( U ./ (Ks .* D.^(2/3)) ).^2;
-
-% end of the function
-return
